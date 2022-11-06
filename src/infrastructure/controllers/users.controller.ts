eb1750a7a8a4a368493dacfd2dcb03a8137733ab
josersn <nettorammos@hotmail.com>
2022-11-06 15:44:31 -0300

    feat: implements provider

--- src/infrastructure/controllers/users.controller.ts
+++ src/infrastructure/controllers/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, POST } from "fastify-decorators";
 import { UserService } from "../../application/services/user-service";
 import { CreateUserUseCase } from "../../application/use-cases/users/create-user/create-user.use-case";
 import { UserRepository } from "../database/prisma/repositories/users-repository.prisma";
+import { Encryption } from "../providers/encryption";
 
 @Controller("users")
 export default class UserController {
@@ -10,10 +11,14 @@ export default class UserController {
     async createUser(req, reply) {
         try {
             const repository = new UserRepository();
-            const service = new UserService(repository);
+            const encryptionProvider = new Encryption();
+            const service = new UserService(repository, encryptionProvider);
             const useCase = new CreateUserUseCase(service);
 
             const user = await useCase.exec(req.body);
+            
+            // @ts-expect-error 
+            delete user.password
 
             reply.status(201).send({
                 message: "User Created",
